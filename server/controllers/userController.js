baab86a2bd40f61b92ba53c909965ba27a7dd2ac
Chanthini Mai <chanthinivenkatesh20@gmail.com>
2026-07-24 15:26:38 +0530

    transporter updation

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -6,18 +6,14 @@ const nodemailer = require("nodemailer");
 
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  connectionTimeout: 10000,
-  greetingTimeout: 10000,
-  socketTimeout: 10000,
 });
-
 // =====================
 // Register User
 // =====================
@@ -128,11 +124,6 @@ const forgotPassword = async (req, res) => {
 
     console.log("Before sendMail");
 
-    console.log("Before verify");
-
-await transporter.verify();
-
-console.log("SMTP connection verified");
 
     await transporter.sendMail({
       from: process.env.EMAIL_USER,
